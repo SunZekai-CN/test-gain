150cdd029cd950e6e78752b9bb233cd46ae39e11
SunZekai-CN <sunzekai@mail.ustc.edu.cn>
2023-02-13 16:36:34 +0800

    update

--- train.py
+++ train.py
@@ -8,12 +8,13 @@ def train(ps_ip,ps_port):
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((ps_ip, ps_port))
     sock.listen(1)
-    lock=threading.Lock()
+
+    client_sock, (client_ip, _) = sock.accept()
     while(True):
         buffer = bytearray()
         while len(buffer) < Message_size:
-            buffer += sock.recv(MAX_RECV_SIZE)
-        sock.send(bytes(Message_size))
+            buffer += client_sock.recv(MAX_RECV_SIZE)
+        client_sock.send(bytes(Message_size))
 
 if __name__ == '__main__':
     ps_ip = sys.argv[1]
