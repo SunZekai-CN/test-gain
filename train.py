54abef5b5cb792cd4ffa45a8ba15ac1a55dbff05
SunZekai-CN <sunzekai@mail.ustc.edu.cn>
2023-02-13 16:43:18 +0800

    update

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import sys
 import socket
 import threading
-from train_worker import Message_size,MAX_RECV_SIZE
+from train_worker import Message_size,MAX_RECV_SIZE,test_time
 
 def train(ps_ip,ps_port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,11 +10,13 @@ def train(ps_ip,ps_port):
     sock.listen(1)
 
     client_sock, (client_ip, _) = sock.accept()
-    while(True):
+    i = 0
+    while(i < test_time):
         buffer = bytearray()
         while len(buffer) < Message_size:
             buffer += client_sock.recv(MAX_RECV_SIZE)
         client_sock.send(bytes(Message_size))
+        i = i +1
 
 if __name__ == '__main__':
     ps_ip = sys.argv[1]
